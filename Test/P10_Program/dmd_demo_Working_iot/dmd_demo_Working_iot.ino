69b28378d91feab57a5d2182abcb2f2b4da15ee4
Ashutosh Karve <89137956+Ashutoshkarve007@users.noreply.github.com>
2022-04-08 16:23:18 +0530

    Add files via upload

---
+++ Test/P10_Program/dmd_demo_Working_iot/dmd_demo_Working_iot.ino
@@ -0,0 +1,263 @@
+ #include <DMD32.h>        
+#include "fonts/SystemFont5x7.h"
+#include "fonts/Arial_black_16.h"
+#include "Secrets.h"
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
+#include <stdio.h>
+#include <string.h>
+
+#define DEBUG true
+#define MODE_1A
+
+#define LTE_PWRKEY_PIN 26
+#define LTE_RESET_PIN 33
+#define LTE_FLIGHT_PIN 27
+
+#define MODEM_RXD 16
+#define MODEM_TXD 17
+
+#define PIN_DMD_nOE  4          //22       //4   // D22 active low Output Enable, setting this low lights all the LEDs in the selected rows. Can pwm it at very high frequency for brightness control.
+#define PIN_DMD_A 13            //19       //13   // D19  default is 15   
+#define PIN_DMD_B 12            //21       //12     // D21
+#define PIN_DMD_CLK 18          //18       //18  // D18_SCK  is SPI Clock if SPI is used   R39
+#define PIN_DMD_SCLK  14        //2        //14    // D02
+#define PIN_DMD_R_DATA    23    //23       //23  // D23_MOSI is SPI Master Out if SPI is used
+
+#define UART_BAUD 115200
+
+//Fire up the DMD library as dmd
+#define DISPLAYS_ACROSS 4
+#define DISPLAYS_DOWN 2
+
+const char* Data = "";
+String from_usb = "";
+
+#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
+#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+DMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);
+WiFiClientSecure net = WiFiClientSecure();
+PubSubClient client(net);
+   //Timer setup
+  //create a hardware timer  of ESP32
+  hw_timer_t * timer = NULL;
+
+/*--------------------------------------------------------------------------------------
+  Interrupt handler for Timer1 (TimerOne) driven DMD refresh scanning, this gets
+  called at the period set in Timer1.initialize();
+--------------------------------------------------------------------------------------*/
+
+
+void IRAM_ATTR triggerScan()
+{
+  dmd.scanDisplayBySPI();
+}
+
+/*--------------------------------------------------------------------------------------
+  setup
+  Called by the Arduino architecture before the main loop begins
+--------------------------------------------------------------------------------------*/
+void setup(void)
+{
+
+// return the clock speed of the CPU
+  Serial.begin(UART_BAUD);
+  delay(100);
+  Serial1.begin(UART_BAUD, SERIAL_8N1, MODEM_RXD, MODEM_TXD);
+    pinMode(LTE_RESET_PIN, OUTPUT);
+    digitalWrite(LTE_RESET_PIN, LOW);
+ 
+    pinMode(LTE_PWRKEY_PIN, OUTPUT);
+    digitalWrite(LTE_RESET_PIN, LOW);
+    delay(100);
+    
+    digitalWrite(LTE_PWRKEY_PIN, HIGH);
+    delay(2000);
+    digitalWrite(LTE_PWRKEY_PIN, LOW);
+ 
+    pinMode(LTE_FLIGHT_PIN, OUTPUT);
+    digitalWrite(LTE_FLIGHT_PIN, LOW); //Normal Mode
+    // digitalWrite(LTE_FLIGHT_PIN, HIGH);//Flight Mode
+ 
+    Serial.println("P10 4G Test Start!");
+  connectAWS();
+  uint8_t cpuClock = ESP.getCpuFreqMHz();
+  
+  // Use 1st timer of 4 
+  // devide cpu clock speed on its speed value by MHz to get 1us for each signal  of the timer
+  timer = timerBegin(0, cpuClock, true);
+  // Attach triggerScan function to our timer 
+  timerAttachInterrupt(timer, &triggerScan, true);
+  // Set alarm to call triggerScan function  
+  // Repeat the alarm (third parameter) 
+  timerAlarmWrite(timer, 300, true);
+
+  // Start an alarm 
+  timerAlarmEnable(timer);
+
+   //clear/init the DMD pixels held in RAM
+   dmd.clearScreen( true );   //true is normal (all pixels off), false is negative (all pixels on)
+   Data = "BeautoSys";
+   sendData("AT+CGMM", 3000, DEBUG);
+//connectAWS();
+
+}
+
+/*--------------------------------------------------------------------------------------
+  loop
+  Arduino architecture main loop
+--------------------------------------------------------------------------------------*/
+void loop(void)
+{
+    while (Serial1.available() > 0)
+    {
+        Serial.write(Serial1.read());
+        
+        yield();
+    }
+     while (Serial.available() > 0)
+    {
+
+        Serial1.write(Serial.read());
+      //  Serial.print(Serial1.read());
+
+
+
+}
+    
+   dmd.clearScreen( true );
+   dmd.selectFont(Arial_Black_16);
+   client.loop();
+
+   dmd.drawMarquee(Data,strlen(Data),(32*DISPLAYS_ACROSS)-1,0);
+   long start=millis();
+   long timer=start;
+   boolean ret=false;
+   while(!ret){
+     if ((timer+30) < millis()) {
+       ret=dmd.stepMarquee(-1,0);
+       timer=millis();
+        client.loop();
+     }
+   }
+}
+void messageHandler(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("incoming: ");
+  Serial.println(topic);
+ 
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, payload);
+  
+   Data = doc["message"];
+  Serial.println(Data);
+//  char json[] = {"message"};
+/*  
+   DeserializationError error = deserializeJson(doc, json);
+     if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+    data = doc["data"];
+    Serial.print(data);
+ */
+}
+void connectAWS()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+ 
+  Serial.println("Connecting to Wi-Fi");
+ int retry = 0;
+  while (WiFi.status() != WL_CONNECTED && retry < 20)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  // Configure WiFiClientSecure to use the AWS IoT device credentials
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+ 
+  // Connect to the MQTT broker on the AWS endpoint we defined earlier
+  client.setServer(AWS_IOT_ENDPOINT, 8883);
+ 
+  // Create a message handler
+  client.setCallback(messageHandler);
+ 
+  Serial.println("Connecting to AWS IOT");
+ 
+  while (!client.connect(THINGNAME) && retry < 20)
+  {
+    Serial.print(".");
+    delay(100);
+  }
+ 
+  if (!client.connected())
+  {
+    Serial.println("AWS IoT Timeout!");
+    return;
+  }
+ 
+  // Subscribe to a topic
+  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+ 
+  Serial.println("AWS IoT Connected!");
+}
+
+bool moduleStateCheck()
+{
+    int i = 0;
+    bool moduleState = false;
+    for (i = 0; i < 5; i++)
+    {
+        String msg = String("");
+        msg = sendData("AT", 1000, DEBUG);
+        if (msg.indexOf("OK") >= 0)
+        {
+            Serial.println("SIM7600 Module had turned on.");
+            moduleState = true;
+            return moduleState;
+        }
+        delay(1000);
+    }
+    return moduleState;
+}
+
+String sendData(String command, const int timeout, boolean debug)
+{
+    String response = "";
+    if (command.equals("1A") || command.equals("1a"))
+    {
+        Serial.println();
+        Serial.println("Get a 1A, input a 0x1A");
+ 
+        //Serial1.write(0x1A);
+        Serial1.write(26);
+        Serial1.println();
+        return "";
+    }
+    else
+    {
+        Serial1.println(command);
+    }
+ 
+    long int time = millis();
+    while ((time + timeout) > millis())
+    {
+        while (Serial1.available())
+        {
+            char c = Serial1.read();
+            response += c;
+        }
+    }
+    if (debug)
+    {
+        Serial.print(response);
+    }
+    return response;
+}
